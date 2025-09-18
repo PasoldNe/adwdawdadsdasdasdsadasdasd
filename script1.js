2d01357478aae17a41edbe5a5e02842e449cd74e
pasoldne <vitorpasold4@gmail.com>
2025-09-17 21:07:30 -0300

    first commit

---
+++ script1.js
@@ -0,0 +1,7 @@
+// Complete o código:
+let meuNome = "Vitor";
+let minhaIdade = 25;
+let minhaCidade = "São Paulo";
+
+// Exiba as informações usando template literals
+console.log(`Meu nome é ${meuNome}, tenho ${minhaIdade} anos e moro em ${minhaCidade}.`);
